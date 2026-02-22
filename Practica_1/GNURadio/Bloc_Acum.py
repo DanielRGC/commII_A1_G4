0fa6ed80ac7edabe499f2971b6deacde96a2713b
JDQG <judaquego89@gmail.com>
2026-02-22 08:58:06 -0500

    Archivos corregidos, listo para merge

--- Practica_1/GNURadio/Bloc_Acum.py
+++ Practica_1/GNURadio/Bloc_Acum.py
@@ -10,7 +10,7 @@
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio import blocks
+from gnuradio import analog
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
@@ -20,12 +20,12 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+import Bloc_Acum_epy_block_0 as epy_block_0  # embedded python block
 import sip
-import untitled_epy_block_1 as epy_block_1  # embedded python block
 
 
 
-class untitled(gr.top_block, Qt.QWidget):
+class Bloc_Acum(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
@@ -48,7 +48,7 @@ class untitled(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "untitled")
+        self.settings = Qt.QSettings("GNU Radio", "Bloc_Acum")
 
         try:
             geometry = self.settings.value("geometry")
@@ -73,7 +73,7 @@ class untitled(gr.top_block, Qt.QWidget):
             2, #number of inputs
             None # parent
         )
-        self.qtgui_time_sink_x_0.set_update_time(5)
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
         self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
 
         self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
@@ -114,20 +114,20 @@ class untitled(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.epy_block_1 = epy_block_1.blk()
-        self.blocks_vector_source_x_0 = blocks.vector_source_f((1,1,1,1,1,1,-1,-1,-1,-1,-1,-1), True, 1, [])
+        self.epy_block_0 = epy_block_0.blk()
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SQR_WAVE, 1000, 1, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_vector_source_x_0, 0), (self.epy_block_1, 0))
-        self.connect((self.blocks_vector_source_x_0, 0), (self.qtgui_time_sink_x_0, 1))
-        self.connect((self.epy_block_1, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.epy_block_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "untitled")
+        self.settings = Qt.QSettings("GNU Radio", "Bloc_Acum")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -139,12 +139,13 @@ class untitled(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
 
 
 
-def main(top_block_cls=untitled, options=None):
+def main(top_block_cls=Bloc_Acum, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
