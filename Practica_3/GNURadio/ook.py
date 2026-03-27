706052b91eb82261cd4a367cb51a69d089cb96c2
DanielRGC <danielricardoguerrerocruz65@gmail.com>
2026-03-27 00:32:13 -0500

    fsk

--- Practica_3/GNURadio/ook.py
+++ Practica_3/GNURadio/ook.py
@@ -7,7 +7,7 @@
 # GNU Radio Python Flow Graph
 # Title: ook
 # Author: radiogis_director
-# GNU Radio version: v3.10.11.0-89-ga17f69e7
+# GNU Radio version: 3.10.9.2
 
 from PyQt5 import Qt
 from gnuradio import qtgui
@@ -29,8 +29,8 @@ import math
 import numpy as np
 import ook_epy_block_0 as epy_block_0  # embedded python block
 import ook_epy_block_0_0 as epy_block_0_0  # embedded python block
+import ook_epy_block_0_1 as epy_block_0_1  # embedded python block
 import sip
-import threading
 
 
 
@@ -57,7 +57,7 @@ class ook(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "ook")
+        self.settings = Qt.QSettings("GNU Radio", "ook")
 
         try:
             geometry = self.settings.value("geometry")
@@ -65,7 +65,6 @@ class ook(gr.top_block, Qt.QWidget):
                 self.restoreGeometry(geometry)
         except BaseException as exc:
             print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
-        self.flowgraph_started = threading.Event()
 
         ##################################################
         # Variables
@@ -75,12 +74,20 @@ class ook(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate = Rb*Sps
         self.h = h = [1]*Sps
         self.fd = fd = Rb
+        self.fc_2 = fc_2 = Rb*2
         self.fc = fc = Rb*4
 
         ##################################################
         # Blocks
         ##################################################
 
+        self._fd_range = qtgui.Range(0, samp_rate/8, samp_rate/1000, Rb, 200)
+        self._fd_win = qtgui.RangeWidget(self._fd_range, self.set_fd, "Deviation Freq", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_grid_layout.addWidget(self._fd_win, 0, 1, 1, 1)
+        for r in range(0, 1):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(1, 2):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self._fc_range = qtgui.Range(0, samp_rate/8, samp_rate/1000, Rb*4, 200)
         self._fc_win = qtgui.RangeWidget(self._fc_range, self.set_fc, "Carrier Freq. ", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._fc_win, 0, 0, 1, 1)
@@ -465,41 +472,49 @@ class ook(gr.top_block, Qt.QWidget):
             self.Menu_grid_layout_3.setColumnStretch(c, 1)
         self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_fff(Sps, h)
         self.interp_fir_filter_xxx_0.declare_sample_delay(0)
-        self._fd_range = qtgui.Range(0, samp_rate/8, samp_rate/1000, Rb, 200)
-        self._fd_win = qtgui.RangeWidget(self._fd_range, self.set_fd, "Deviation Freq", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._fd_win, 0, 1, 1, 1)
-        for r in range(0, 1):
+        self._fc_2_range = qtgui.Range(0, samp_rate/8, samp_rate/1000, Rb*2, 200)
+        self._fc_2_win = qtgui.RangeWidget(self._fc_2_range, self.set_fc_2, "Carrier Freq. 2", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_grid_layout.addWidget(self._fc_2_win, 1, 0, 1, 1)
+        for r in range(1, 2):
             self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(1, 2):
+        for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
+        self.epy_block_0_1 = epy_block_0_1.blk()
         self.epy_block_0_0 = epy_block_0_0.blk()
         self.epy_block_0 = epy_block_0.blk(fc=fc, samp_rate=samp_rate)
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_ff(2)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff((2*math.pi*fd/(Rb*Sps)))
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
+        self.blocks_add_const_vxx_0 = blocks.add_const_ff((-0.5))
         self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 2, 1000000))), True)
-        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, (math.pi/2))
+        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 1)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_const_source_x_0, 0), (self.epy_block_0, 1))
-        self.connect((self.analog_const_source_x_0, 0), (self.epy_block_0_0, 1))
+        self.connect((self.analog_const_source_x_0, 0), (self.epy_block_0, 0))
+        self.connect((self.analog_const_source_x_0, 0), (self.epy_block_0_0, 0))
         self.connect((self.analog_random_source_x_0, 0), (self.blocks_char_to_float_0, 0))
-        self.connect((self.blocks_char_to_float_0, 0), (self.interp_fir_filter_xxx_0, 0))
-        self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_0_0, 0))
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
+        self.connect((self.blocks_char_to_float_0, 0), (self.blocks_add_const_vxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.epy_block_0, 1))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.epy_block_0_0, 1))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.interp_fir_filter_xxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.qtgui_time_sink_x_0_0, 0))
         self.connect((self.epy_block_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_0_1_0, 1))
         self.connect((self.epy_block_0_0, 0), (self.qtgui_const_sink_x_0, 0))
         self.connect((self.epy_block_0_0, 0), (self.qtgui_freq_sink_x_0_0, 0))
         self.connect((self.epy_block_0_0, 0), (self.qtgui_time_sink_x_0_1_0_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.epy_block_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.epy_block_0_0, 0))
+        self.connect((self.epy_block_0_1, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.interp_fir_filter_xxx_0, 0), (self.epy_block_0_1, 0))
         self.connect((self.interp_fir_filter_xxx_0, 0), (self.qtgui_time_sink_x_0_1, 0))
         self.connect((self.interp_fir_filter_xxx_0, 0), (self.qtgui_time_sink_x_0_1_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "ook")
+        self.settings = Qt.QSettings("GNU Radio", "ook")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -513,6 +528,7 @@ class ook(gr.top_block, Qt.QWidget):
         self.Sps = Sps
         self.set_h([1]*self.Sps)
         self.set_samp_rate(self.Rb*self.Sps)
+        self.blocks_multiply_const_vxx_0.set_k((2*math.pi*self.fd/(self.Rb*self.Sps)))
 
     def get_Rb(self):
         return self.Rb
@@ -520,8 +536,10 @@ class ook(gr.top_block, Qt.QWidget):
     def set_Rb(self, Rb):
         self.Rb = Rb
         self.set_fc(self.Rb*4)
+        self.set_fc_2(self.Rb*2)
         self.set_fd(self.Rb)
         self.set_samp_rate(self.Rb*self.Sps)
+        self.blocks_multiply_const_vxx_0.set_k((2*math.pi*self.fd/(self.Rb*self.Sps)))
         self.qtgui_time_sink_x_0_0.set_samp_rate(self.Rb)
 
     def get_samp_rate(self):
@@ -548,6 +566,13 @@ class ook(gr.top_block, Qt.QWidget):
 
     def set_fd(self, fd):
         self.fd = fd
+        self.blocks_multiply_const_vxx_0.set_k((2*math.pi*self.fd/(self.Rb*self.Sps)))
+
+    def get_fc_2(self):
+        return self.fc_2
+
+    def set_fc_2(self, fc_2):
+        self.fc_2 = fc_2
 
     def get_fc(self):
         return self.fc
@@ -566,7 +591,6 @@ def main(top_block_cls=ook, options=None):
     tb = top_block_cls()
 
     tb.start()
-    tb.flowgraph_started.set()
 
     tb.show()
 
